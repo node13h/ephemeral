a15297820686089e7ebd3d91270e67d150fda11c
Sergej Alikov <sergej.alikov@gmail.com>
2019-06-24 23:34:35 +0100

    Dynamically set number of wworkers, configurable bind address/port

--- ephemeral.sh
+++ ephemeral.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
-gunicorn --bind=0.0.0.0:3000 --workers=2 ephemeral.wsgi
+BIND="${1:-0.0.0.0:8080}"
+
+num_cores=$(nproc)
+num_workers=$((2 * num_cores + 1))
+
+exec gunicorn --bind="$BIND" --workers="$num_workers" ephemeral.wsgi
